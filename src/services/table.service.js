8ce43b06c047316241cf4649cf4cfb4b11bddfc7
NaufalAulia <naufal7600@gmail.com>
2023-01-10 16:43:12 +0700

    Add Server Integration for Table feature

--- src/services/table.service.js
+++ src/services/table.service.js
@@ -1,80 +1,73 @@
-import tableData from "../shared/fixtures/table.json";
+import {ErrorResponse} from "../shared/dtos/index.js";
 
-export default function tableService() {
-    const tables = [...tableData];
-    let id = tables[tables.length - 1].id;
+export default function tableService(http) {
+    const baseURI = "tables";
 
     const addTable = async (dto) => {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                const newTable = {
-                    id: (++id).toString(),
-                    name: dto.name,
-                    availability: dto.availability,
-                };
-
-                tables.push(newTable);
-                resolve(newTable);
-            }, 1000);
-        });
+        try {
+            const res = await http.post(`${baseURI}`, dto);
+            return res.data.data;
+        } catch (error) {
+            throw new ErrorResponse(
+                error.response.data.code || error.response.status,
+                error.response.data.message || error.message,
+                error.response.data.reason || error.message
+            );
+        }
     };
 
     const getTable = async (id) => {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                const table = tables.find(t => t.id === Number(id));
-                if (!table) {
-                    return reject(new Error(`Table with ID ${id} Not Found`));
-                }
-
-                return resolve(table);
-            }, 1000);
-        });
+        try {
+            const res = await http.get(`${baseURI}/${id}`);
+            return res.data.data;
+        } catch (error) {
+            console.log(error);
+            throw new ErrorResponse(
+                error.response.data.code || error.response.status,
+                error.response.data.message || error.message,
+                error.response.data.reason || error.message
+            );
+        }
     }
 
     const listTable = async (page, size) => {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                const offset = page * size;
-                let data;
-                if (size === 0) {
-                    data = [...tables];
-                } else {
-                    data = tables.slice(offset, offset + size);
-                }
-
-                resolve({
-                    page,
-                    size,
-                    data,
-                    count: data.length,
-                    totalPage: Math.ceil(tables.length / size),
-                    totalCount: tables.length,
-                });
-            }, 1000);
-        });
+        try {
+            const res = await http.get(`${baseURI}?page=${page}&size=${size}`);
+            return res.data.data;
+        } catch (error) {
+            console.log(error);
+            throw new ErrorResponse(
+                error.response.data.code || error.response.status,
+                error.response.data.message || error.message,
+                error.response.data.reason || error.message
+            );
+        }
     };
 
     const updateTable = async (id, updatedTable) => {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                const updatedIndex = tables.findIndex((t) => t.id === id);
-                tables[updatedIndex] = { ...updatedTable };
-
-                resolve(tables[updatedIndex]);
-            }, 500);
-        });
+        try {
+            const res = await http.put(`${baseURI}/${id}`, updatedTable);
+            return res.data.data;
+        } catch (error) {
+            throw new ErrorResponse(
+                error.response.data.code || error.response.status,
+                error.response.data.message || error.message,
+                error.response.data.reason || error.message
+            );
+        }
     };
 
     const removeTable = async (id) => {
-        console.log(id);
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                const deletedIndex = tables.findIndex(t => t.id === id);
-                console.log(deletedIndex);
-                resolve(tables.splice(deletedIndex, 1)[0].id);
-            }, 1000);
-        });
+        try {
+            const res = await http.delete(`${baseURI}/${id}`);
+            return res.data.data;
+        } catch (error) {
+            throw new ErrorResponse(
+                error.response.data.code || error.response.status,
+                error.response.data.message || error.message,
+                error.response.data.reason || error.message
+            );
+        }
     }
 
     return {addTable, listTable, getTable, updateTable, removeTable};
